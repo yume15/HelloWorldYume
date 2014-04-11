444d5e6227f27a4809f7f95690b690c8a3b3d89b
g12915iy <g12915iy@s30430.tsuda.ac.jp>
2014-04-11 11:04:37 +0900

    新規にHelloWorldYumeを作成しました

---
+++ src/helloworldyume/HelloWorldYume.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworldyume;
+
+/**
+ *
+ * @author g12915iy
+ */
+public class HelloWorldYume {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
